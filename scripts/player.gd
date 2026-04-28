d8a211b485301a220dca5ee60f35a79c5e4323df
Martin Mikestik <martin.mikestik@gmail.com>
2026-04-28 19:15:47 +0200

    Make player able to move in level

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+@export var speed: float = 300.0
+
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("Left", "Right", "Up", "Down")
+	
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, speed)
+
+	move_and_slide()
