828e43301d570f22eebdee50bd81cd44a8053d28
Martin Mikestik <martin.mikestik@gmail.com>
2026-04-29 23:07:10 +0200

    add energy gui with adding and removing interface

--- scripts/energy_bar.gd
+++ scripts/energy_bar.gd
@@ -5,6 +5,7 @@ const ENERGY_SLOT = preload("uid://bkm6mdr635hnw")
 
 var list_energy_slots: Array[ProgressBar] = []
 var num_slots: int = 0
+var filled_slots: int = 0
 
 func _ready():
 	connect_all_signals()
@@ -18,16 +19,37 @@ func handle_key_presses(key_press: String):
 		add_energy_slots(1)
 	if key_press == "0":
 		add_energy_slots(3)
+	if key_press == "2":
+		fill_energy_slots()
 
 func add_energy_slots(num: int):
 	for i in range(num):
 		var energy_slot_instance = ENERGY_SLOT.instantiate()
 		h_box_container.add_child(energy_slot_instance)
-		list_energy_slots.insert(0, energy_slot_instance)
+		list_energy_slots.append(energy_slot_instance)
 		num_slots += 1
+		recalculate_filled_slots()
 
-func fill_energy_slots(num: int):
-	pass
+func fill_energy_slots(): # num: int
+	if filled_slots < num_slots:
+		#var slot_to_fill_index: int = num_slots - filled_slots - 1
+		var slot_to_fill_index: int = filled_slots
+		#list_energy_slots[slot_to_fill_index].value = list_energy_slots[slot_to_fill_index].max_value
+		filled_slots += 1
+		print("here")
+		recalculate_filled_slots()
+		
+
+func recalculate_filled_slots():
+	print("filled slots: " + str(filled_slots))
+	print("num slots: " + str(num_slots))
+	for i in range(0, num_slots - filled_slots):
+		list_energy_slots[i].value = list_energy_slots[i].min_value
+		print("adding zero energy to: " + list_energy_slots[i].name + " slot")
+		#print("setting: " + str(i) + " to max")
+	for i in range(num_slots - filled_slots, num_slots):
+		list_energy_slots[i].value = list_energy_slots[i].max_value
+		print("adding max energy to: " + list_energy_slots[i].name + " slot")
 
 # Interface:
 # - increase/descrease maximum number of slots by n
