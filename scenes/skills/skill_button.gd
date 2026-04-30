c44e0c50851321a381dcfb796968d3eb4c8c2a42
Martin Mikestik <martin.mikestik@gmail.com>
2026-04-30 17:07:35 +0200

    add buttons to level scene

--- scenes/skills/skill_button.gd
+++ scenes/skills/skill_button.gd
@@ -3,9 +3,36 @@ extends Panel
 var is_active: bool = false
 
 
+var new_stylebox = StyleBoxFlat.new()
+var inactive_stylebox: StyleBox = null
+@export var original_color: Color = Color(0, 0, 0, 0)
+@export var active_color: Color = Color(0, 0, 0, 0)
+
+@onready var label: Label = $SkillButtonForeground/Label
+
+@export var my_text: String = ""
+
+func _ready():
+	label.text = my_text
+	inactive_stylebox = get_theme_stylebox("panel")
+	original_color = inactive_stylebox.bg_color
+
 func _process(_delta):
-	#handle_visuals()
-	pass
+
+	if Input.is_action_just_pressed("StartSkills"):
+		make_active()
+	if Input.is_action_just_released("StartSkills"):
+		make_passive()
+
+
 
 func make_active():
-	pass
+	#add_theme_stylebox_override("panel", new_stylebox)
+	inactive_stylebox.bg_color = active_color
+
+func make_passive():
+	inactive_stylebox.bg_color = original_color
+	#add_theme_stylebox_override("panel", inactive_stylebox)
+
+func set_off():
+	print("Button " + name + " was set off.")
--- scripts/skills.gd
+++ scripts/skills.gd
@@ -2,20 +2,37 @@ extends Control
 
 var l_active: bool = false
 
+@onready var skill_button: Panel = $HBoxContainer/SkillButton
+@onready var skill_button_2: Panel = $HBoxContainer/SkillButton2
+@onready var skill_button_3: Panel = $HBoxContainer/SkillButton3
+
+
+var num_to_key: Dictionary[int, Panel] = {}
 
 func _ready():
-	connect_all_signals()
 	process_input()
-	handle_visuals_skill_button()
+	connect_all_signals()
+	num_to_key = {
+	1: skill_button,
+	2: skill_button_2,
+	3: skill_button_3
+}
+
+func _process(_delta):
+	process_input()
 
 func connect_all_signals():
-	pass
+	InputHandler.key_press.connect(handle_key_presses)
+
+func handle_key_presses(key_press: String):
+	if l_active:
+		if key_press.is_valid_int():
+			var key_i: int = int(key_press)
+			if key_i in num_to_key.keys():
+				num_to_key.get(int(key_press)).set_off()
 
 func process_input():
 	if Input.is_action_just_pressed("StartSkills"):
 		l_active = true
 	if Input.is_action_just_released("StartSkills"):
 		l_active = false
-
-func handle_visuals_skill_button():
-	
