8e2b01f44868a7ed90cf0b4aabd74edd983ac4a3
Martin Mikestik <martin.mikestik@gmail.com>
2026-05-02 16:41:33 +0200

    polish skill buttons

--- scripts/skills_scripts/skill_button.gd
+++ scripts/skills_scripts/skill_button.gd
@@ -1,19 +1,10 @@
 extends Panel
 
+@export var verbose: bool = false
+
+
 @export var is_unlocked: bool = false
 var is_l_pressed: bool = false
-
-var new_stylebox = StyleBoxFlat.new()
-var inactive_stylebox: StyleBox = null
-@export var original_color: Color = Color(0, 0, 0, 0)
-@export var active_color: Color = Color(0, 0, 0, 0)
-
-@onready var label: Label = $SkillButtonForeground/Label
-
-@export var my_text: String = ""
-@export var necessary_button: String = "2"
-
-@export var necessary_energy: int = 3
 
 # cooldown
 var current_time: int = 0
@@ -21,8 +12,16 @@ var current_time: int = 0
 var last_time_used: float = 0 # in unix milliseconds
 var cooldown_expired: bool = false
 
+@onready var label: Label = $SkillButtonForeground/Label
+@export var my_text: String = ""
+@export var necessary_button: String = "2"
+@export var necessary_energy: int = 3
+
 # visual stuff
 var my_material: Material = null
+@export var original_color: Color = Color(0, 0, 0, 0)
+@export var active_color: Color = Color(0, 0, 0, 0)
+var inactive_stylebox: StyleBox = null
 
 func _ready():
 	connect_all_signals()
@@ -42,10 +41,8 @@ func _process(_delta):
 	handle_visuals()
 
 func handle_user_input():
-	if Input.is_action_just_pressed("StartSkills"):
-		make_active()
-	if Input.is_action_just_released("StartSkills"):
-		make_passive()
+	is_l_pressed = Input.is_action_pressed("StartSkills")
+
 
 func handle_key_presses(key_press: String):
 	if key_press == necessary_button:
@@ -54,35 +51,37 @@ func handle_key_presses(key_press: String):
 func recalculate_flags():
 	current_time = Time.get_unix_time_from_system() * 1000
 	cooldown_expired = current_time - last_time_used > cooldown_time * 1000
+	if can_set_off():
+		inactive_stylebox.bg_color = active_color
+	else:
+		inactive_stylebox.bg_color = original_color
 
 func handle_visuals():
 	update_cooldown_shader()
 
-func make_active():
-	is_l_pressed = true
-	#set_off()
-	inactive_stylebox.bg_color = active_color
-
-func make_passive():
-	is_l_pressed = false
-	inactive_stylebox.bg_color = original_color
 
 
 func try_set_off():
-	print("is unlocked? " + str(is_unlocked))
-	print("cooldown expired? " + str(cooldown_expired))
-	print("energy? " + str(get_user_energy()) + "/" + str(necessary_energy))
-	print("l pressed? " + str(is_l_pressed))
+	if can_set_off():
+		set_off()
+
+
+
+func can_set_off():
+	if verbose:
+		print("is unlocked? " + str(is_unlocked))
+		print("cooldown expired? " + str(cooldown_expired))
+		print("energy? " + str(get_user_energy()) + "/" + str(necessary_energy))
+		print("l pressed? " + str(is_l_pressed))
 	if is_unlocked and\
 	cooldown_expired and\
 	get_user_energy() >= necessary_energy and\
 	is_l_pressed:
-		set_off()
-		#pass
-		
+		return true
+	else:
+		return false
 
 func set_off():
-	print("REALLY?")
 	# update flags like cooldown
 	# take away player's energy
 	# perform the effect
@@ -95,14 +94,18 @@ func get_user_energy():
 	if GameManager.player:
 		return GameManager.player.current_energy
 	else:
-		print("No player attached. (get user energy)")
+		if verbose:
+			print("No player attached. (get user energy)")
 		return 1000
 
+
+# TODO: later, let the actual skill delete the user's energy, not the button
 func deplete_user_energy():
 	if GameManager.player:
 		GameManager.player.deplete_energy_slots(necessary_energy)
 	else:
-		print("No player attached. (deplete)")
+		if verbose:
+			print("No player attached. (deplete)")
 
 #region Visual Aspects
 
